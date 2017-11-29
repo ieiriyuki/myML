154c87a4587db2ef7296647fbaecacce20d4e526
ieiriyuki <ieiriyuki@sur.bbiq.jp>
2017-11-29 13:55:14 +0900

    finished evaluation part

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 """
     Starter code for the evaluation mini-project.
     Start by copying your trained/tested POI identifier from
@@ -28,4 +27,30 @@ labels, features = targetFeatureSplit(data)
 
 ### your code goes here 
 
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+#clf.fit(features, labels)
+#print clf.score(features, labels)
+
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(
+  features, labels, test_size=0.3, random_state=42)
+
+clf.fit(features_train, labels_train)
+
+print "accuracy of train: ", clf.score(features_train, labels_train)
+print "accuracy of test: ", clf.score(features_test, labels_test)
+
+pred = clf.predict(features_test)
+
+print len(labels_test)
+print labels_test
+print pred
+
+print pred * labels_test 
+
+from sklearn import metrics as mtr
+print "accuracy: ", mtr.accuracy_score(labels_test, pred)
+print "precision: ", mtr.precision_score(labels_test, pred)
+print "recall: ", mtr.recall_score(labels_test, pred)
 
