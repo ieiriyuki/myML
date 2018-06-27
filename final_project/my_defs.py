406cdfaa75425fb05e379e9d6fec9298780f9a74
ieiriyuki <ieiriyuki@sur.bbiq.jp>
2018-06-27 15:13:44 +0900

    add my_defs.py

---
+++ final_project/my_defs.py
@@ -0,0 +1,16 @@
+#!usr/bin/python
+
+def getFrac(nom, denom):
+    frac = 0.
+    if denom == 'NaN':
+        return frac
+    elif denom == 0
+        return frac
+    else:
+        if nom == 'NaN'
+            return frac
+        else:
+            frac = float(nom)/denom
+    return frac
+
+#end of file
